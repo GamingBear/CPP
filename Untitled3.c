5f9b08931ec961a57acca3a7620f862253878fcd
GamingBear <78107190+GamingBear@users.noreply.github.com>
2021-12-09 10:23:05 +0100

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    int strA=5;
+    int strB=8;
+    int strC=2;
+    printf("Unes strance:\n");
+    scanf("%d %d %d",&strA,&strB,&strC);
+    printf("Volumen je %d cm^3",strA*strB*strC);
+}
