7deaec51e3970d31e36fcf688421f36270de5df1
GamingBear <78107190+GamingBear@users.noreply.github.com>
2022-01-13 08:43:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
-int main()
-{
-    float stara_cijena;
-    float nova_cijena;
-    float postotak_snizenja;
+int main(){
 
-    printf("Unesi staru cijenu: ");
-    scanf("%f", &stara_cijena);
-    printf("Unesi novu cijenu: ");
-    scanf("%f", &nova_cijena);
+    int d;
 
-    postotak_snizenja=(stara_cijena-nova_cijena)/stara_cijena*100;
-    printf("\nCijena je snizena za %.0f %% \n", postotak_snizenja);
+    printf("Unesi promjer kotaca:\n");
+    scanf("%d", &d);
 
+    float opseg=d*3.1356;
 
+    printf("Napravi %.0f okretaja", 400000/opseg);
 
-
-return 0;
 }
