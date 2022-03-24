8bbda18ac4408e3fbe7c3b51abc331ec5d82f142
GamingBear <78107190+GamingBear@users.noreply.github.com>
2022-03-24 10:14:12 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n, i, fac=1, br=0;
+
+    do{
+    printf("Unesi broj\n");
+    scanf("%d", &n);
+    if(n<0){printf("Error\n");
+    if(br>5) printf("Unesi pozitivan %d\n",br);
+    }
+    br++;
+
+    }while (n<0);
+    i=n;
+    while(i)
+        fac*=i++;
+    printf("faktoriel broja %d je %d",n, fac);
+}
