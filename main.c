5a8c2b5d169433da0aa88343b7ffa6a7e7dad639
GamingBear <78107190+GamingBear@users.noreply.github.com>
2022-05-26 10:17:34 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-
-
+#include <ctype.h>
+#include <stdlib.h>
 int main(){
-char odabir[8];
-int nestojeodabrano=1;
-float sum=0;
-int i;
 
-while(nestojeodabrano){
-system("cls");
-printf("\t\t Meni:\n");
-printf("\t\t 1 kava\n");
-printf("\t\t 2 sladoled\n");
-printf("\t\t 3 govno\n");
-printf("\t\t 4 krofna\n");
-printf("Za prestanak unosa unesite bilo sto drugo\n");
+    char odabir[8];
+    int nestojeodabrano=1;
+    float sum=0;
+    int i;
+    int count=0;
+    int racunGotov=0;
 
-scanf("%s",odabir);
+    while(nestojeodabrano){
+            racunGotov=1;
+    system("cls");
+    printf("Racun broj %d\t",++count );
+    printf("Promet: %.2f\n", sum);
+    printf("1. Kava: 120kn\n");
+    printf("2. Sladoled: 1,5kn\n");
+    printf("3. Govno: 25kn\n");
+    printf("4. Krofna: 1500kn\n");
+    printf("Za prestanak unosa unesite bilo sto drugo!\n");
 
 
-for( i=0; odabir[1]!='\0';i++)
-    odabir[i]=tolower(odabir[i]);
+    while(racunGotov){
+            printf("Za kraj racuna upisite gotovo\n");
+    printf("Odabir: ");
+    scanf("%s", odabir);
 
-if(!strcmp(odabir,"kava")|!strcmp(odabir,"1")) sum+=120;
-else if(!strcmp(odabir,"sladole")|!strcmp(odabir,"2")) sum+=1.5;
-else if(!strcmp(odabir,"govno")|!strcmp(odabir,"3")) sum+=25;
-else if(!strcmp(odabir,"krofna")|!strcmp(odabir,"4")) sum+=1500;
-else nestojeodabrano=0;
+    for(i=0;odabir[i]!='\0';i++)
+        odabir[i]=tolower(odabir[i]);
 
+    if(!strcmp(odabir,"kava")|!strcmp(odabir,"1")) sum+=120;
+    else if (!strcmp(odabir,"sladoled")|!strcmp(odabir,"2")) sum+=1.5;
+    else if (!strcmp(odabir,"govno")|!strcmp(odabir,"3")) sum+=25;
+    else if (!strcmp(odabir,"krofna")|!strcmp(odabir,"4")) sum+=1500;
+    else if (!strcmp(odabir,"gotovo"))racunGotov=0;
+    else nestojeodabrano=0;
+    }
 }
-
-printf("ukupno promet danas iznosi %.2f",sum);
+printf("Ukupan promet danas iznosi %.2f", sum);
 }
