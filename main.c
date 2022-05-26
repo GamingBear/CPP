3d287538b88452be3e0dc66646e46e865d730a67
GamingBear <78107190+GamingBear@users.noreply.github.com>
2022-05-26 09:34:30 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main()
-{
+
+int main(){
+char odabir[8];
+int nestojeodabrano=1;
+float sum=0;
 int i;
-int x[6];
 
-for(i=0;i<6;i++){
+while(nestojeodabrano){
+system("cls");
+printf("\t\t Meni:\n");
+printf("\t\t 1 kava\n");
+printf("\t\t 2 sladoled\n");
+printf("\t\t 3 govno\n");
+printf("\t\t 4 krofna\n");
+printf("Za prestanak unosa unesite bilo sto drugo\n");
 
-    printf("Unesi broj");
-    scanf("%d",&x[i]);
+scanf("%s",odabir);
+
+
+for( i=0; odabir[1]!='\0';i++)
+    odabir[i]=tolower(odabir[i]);
+
+if(!strcmp(odabir,"kava")|!strcmp(odabir,"1")) sum+=120;
+else if(!strcmp(odabir,"sladole")|!strcmp(odabir,"2")) sum+=1.5;
+else if(!strcmp(odabir,"govno")|!strcmp(odabir,"3")) sum+=25;
+else if(!strcmp(odabir,"krofna")|!strcmp(odabir,"4")) sum+=1500;
+else nestojeodabrano=0;
+
 }
 
-printf("\n");
-
-for(i=0;i<6;i++){
-    printf("%d\n",x[i]);
-}
-    return 0;
+printf("ukupno promet danas iznosi %.2f",sum);
 }
